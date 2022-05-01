75ad351c271ae04f53024a7eb711d789581d1210
Andrew Hinson <hinsonan@gmail.com>
2022-05-01 15:25:31 -0500

    final changes

--- main.py
+++ main.py
@@ -17,33 +17,45 @@ if __name__ == '__main__':
     model.eval()
     # load image
     test_image = Path('data/WIDER_val/images/52--Photographers/52_Photographers_photographertakingphoto_52_807.jpg')
-    img = Image.open(test_image)
-    boxes = detect.detector.detect(np.array(img))
+    dirs = os.listdir('data/two_stage')
+    actual = []
+    predicted = []
+    for dir in dirs:
+        for file in os.listdir(f'data/two_stage/{dir}'):
+            img = Image.open(f'data/two_stage/{dir}/{file}')
+            boxes = detect.detector.detect(np.array(img))
+            print(file)
+            # draw box
+            for box in boxes:
+                # clip the bounding box dimensions to the image size
+                box = np.clip(box,0,img.size[0]-2)
+                box = [int(x) for x in box]
+                img_draw = ImageDraw.Draw(img)
+                img_draw.rectangle(box,outline='blue',width=5)
+            # img_draw.rectangle([338, 26, 338+521, 26+720],outline='green',width=8)
+            im1 = img.crop(box)
 
-    # draw box
-    for box in boxes:
-        # clip the bounding box dimensions to the image size
-        box = np.clip(box,0,img.size[0]-2)
-        box = [int(x) for x in box]
-        img_draw = ImageDraw.Draw(img)
-        img_draw.rectangle(box,outline='blue',width=5)
-    img_draw.rectangle([338, 26, 338+521, 26+720],outline='green',width=8)
-    im1 = img.crop(box)
-
-    # resize and greyscale image
-    im1 = ImageOps.grayscale(im1)
-    transform = transforms.Compose([
-            transforms.Resize(size=(128,128)),
-            transforms.ToTensor(),
-            # note that this may not be the most accurate way to normalize
-            transforms.Normalize(mean=[0.5],std=[0.5])
-        ])
-    im1 : torch.Tensor = transform(im1)
-    im1 = im1.unsqueeze(0)
-    out = model(im1)
-    label = np.argmax(out.detach().numpy())
-    dic = {0:'anger',1:'contempt',2:'disgust',3:'fear',4:'happiness',5:'neutrality',6:'sadness',7:'surprise'}
-    string_label = dic[label]
-    # img_draw.text((box[0],box[1]),string_label,(255,255,255),ImageFont.truetype(f'fonts{os.sep}US101.TTF',size=65))
-    img.show()
+            # resize and greyscale image
+            im1 = ImageOps.grayscale(im1)
+            transform = transforms.Compose([
+                    transforms.Resize(size=(128,128)),
+                    transforms.ToTensor(),
+                    # note that this may not be the most accurate way to normalize
+                    transforms.Normalize(mean=[0.5],std=[0.5])
+                ])
+            im1 : torch.Tensor = transform(im1)
+            im1 = im1.unsqueeze(0)
+            out = model(im1)
+            label = np.argmax(out.detach().numpy())
+            dic = {0:'anger',1:'contempt',2:'disgust',3:'fear',4:'happiness',5:'neutrality',6:'sadness',7:'surprise'}
+            string_label = dic[label]
+            actual.append(dir)
+            predicted.append(string_label)
+            img_draw.text((box[0],box[1]),string_label,(255,255,255),ImageFont.truetype(f'fonts{os.sep}US101.TTF',size=65))
+            img.show()
+    count = 0
+    for x,y in zip(actual,predicted):
+        if x != y:
+            count += 1
+    print((len(actual) - count) / len(actual) )
 
--- plot_helpers.py
+++ plot_helpers.py
@@ -9,4 +9,36 @@ def plot_learning_curve(train_data:list,val_data:list,title:str,output_path:str)
     ax.set_ylabel('Score')
     ax.set_xlabel('Epoch')
     plt.savefig(output_path)
+
+if __name__ == '__main__':
+    import json
+    vgg_init = 'experiment_results/vgg_initial/metrics.json'
+    vgg_augment = 'experiment_results/vgg_with_augment/metrics.json'
+
+    res_init = 'experiment_results/resnet18_initial/metrics.json'
+    res_augment = 'experiment_results/resnet18_with_augmentation/metrics.json'
+
+    with open(vgg_init, 'r') as f:
+        vgg_init_data = json.load(f) 
     
+    with open(res_init, 'r') as f:
+        res_init_data = json.load(f) 
+
+    with open(vgg_augment, 'r') as f:
+        vgg_augment_data = json.load(f) 
+
+    with open(res_augment, 'r') as f:
+        res_augment_data = json.load(f) 
+
+    plt.plot(vgg_init_data['train_loss'])
+    plt.plot(vgg_init_data['val_loss'])
+    plt.plot(vgg_init_data['train_acc'])
+    plt.plot(vgg_init_data['val_acc'])
+    plt.plot(vgg_init_data['train_precision'])
+    plt.plot(vgg_init_data['val_precision'])
+    plt.plot(vgg_init_data['train_recall'])
+    plt.plot(vgg_init_data['val_recall'])
+    plt.legend(['train_loss','val_loss','train_accuracy','val_accuracy','train_precision','val_precision','train_recall','val_recall'],loc='upper right')
+    plt.ylabel('Score')
+    plt.xlabel('Epoch')
+    plt.show()
